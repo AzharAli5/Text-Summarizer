f799b7fc55f54aafff4a9b4dd6dcbe89faa16ee7
AzharAli05 <md.azhar4623@gmail.com>
2024-12-26 12:06:58 -0500

    requirements & installation done

---
+++ src/textSummarizer/utils/common.py
@@ -0,0 +1,29 @@
+import os 
+from box.exceptions import BoxValueError
+import yaml
+from textSummarizer.logging import logger
+from ensure import ensure_annotations
+from box import ConfigBox
+from pathlib import Path
+from typing import Any
+
+@ensure_annotations
+def read_yaml(path_to_yaml)-> ConfigBox:
+    """read yaml files and returns
+    Arg: 
+        path_to_yaml(str): path like input 
+    Raises:
+         ValueError: if yaml file is empty
+         e: empty file 
+    Returns:
+         ConfigBox: Config type
+    """
+    try:
+        with open(path_to_yaml) as yaml_file:
+            content = yaml.safe_load(yaml_file)
+            logger.info(f"yaml file :{path_to_yaml} loaded successfully")
+            return ConfigBox(content)
+    except BoxValueError:
+        raise ValueError("yaml file is empty")
+    except Exception as e:
+        raise e
